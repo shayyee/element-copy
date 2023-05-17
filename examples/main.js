1b435457375a7fe14e0919fd09a1c911e785d18c
shayyee <xxy1024235202@163.com>
2023-05-17 22:20:50 +0800

    feat: docs打包

--- examples/main.js
+++ examples/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import MeUI from '../lib/me-vue-ui.common'
+import MeUI from '../src/index'
 import router from './router'
 import hljs from 'highlight.js'
 import demoBlock from './components/demo-block.vue'
